832a8f770b8f44cf9300b9ada58379bd3548d0a8
Liang Lin <lianglin@outlook.com>
2016-09-11 13:58:31 +0100

    add py solution

--- codility/PassingCars/PassingCars.py
+++ codility/PassingCars/PassingCars.py
@@ -2,6 +2,19 @@
 # Email: lianglin@outlook.com
 # Date: 2016-04-01
 class Solution:
-  def solution(self, A):
-  pass
-  
+    def solution(self, A):
+        n = len(A)
+        counter = 0
+        eastcar = 0
+        threshold = 1000000000
+        for i in range(n):
+            if A[i] == 0:
+                eastcar += 1
+            else:
+                counter += eastcar
+                if counter > threshold:
+                     return -1
+        return counter
+
+sol = Solution()
+print(sol.solution([0,1,0,1,1,0,1]))
