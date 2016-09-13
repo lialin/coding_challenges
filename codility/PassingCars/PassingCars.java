a98fbdff798601ed7d4fc398e88f0893b5ff161c
Liang Lin <lianglin@outlook.com>
2016-09-14 00:49:23 +0100

    add java solution

--- codility/PassingCars/PassingCars.java
+++ codility/PassingCars/PassingCars.java
@@ -5,6 +5,19 @@
  */
 Class Solution {
     public int solution(int[] A) {
-
+        int counter = 0;
+        int eastcar = 0;
+        int threshold = 1000000000;
+        for (int i = 0; i < A.length; i++) {
+            if(A[i] == 0) {
+                eastcar ++;
+            } else {
+                counter += eastcar;
+                if(counter > threshold) {
+                    return -1;
+                }
+            }
+        }
+        return counter;
     }
 }
