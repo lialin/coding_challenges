4c8c0e9a54651034b51bb80ca5821e84d2704316
Liang Lin <lianglin@outlook.com>
2016-09-14 03:12:14 +0100

    commit solution

--- codility/CountDiv/CountDiv.java
+++ codility/CountDiv/CountDiv.java
@@ -4,7 +4,11 @@
  * Date: 2016-09-14
  */
 Class Solution {
-    public int solution(int[] A) {
-
+    public int solution(int A, int B, int K) {
+        int counter = B/K - A/K;
+        if(A%K == 0) {
+            return counter + 1;
+        }
+        return counter;
     }
 }
