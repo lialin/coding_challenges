ad547eb891b063791ca65f9475e0140e6a5d24a0
Liang Lin <lianglin@outlook.com>
2016-09-14 02:59:54 +0100

    add py solution

--- codility/CountDiv/CountDiv.py
+++ codility/CountDiv/CountDiv.py
@@ -3,4 +3,7 @@
 # Date: 2016-09-14
 class Solution:
     def solution(self, A, B, K):
-        
+        if A % K == 0:
+            return B/K - A/K + 1
+        else:
+            return B/K - A/K
